6b788f7c19ff3e031d72f8804c851d605ab28bb5
云飞 左 <zuoyunfei@twt.edu.cn>
2019-08-22 14:28:21 +0800

    Readme

--- src/Api.jsx
+++ src/Api.jsx
@@ -1,5 +1,5 @@
 export default {
-    "api_key": "V_Q_pgEIWm1-1WWkoyyu21grRdA60gYa",
-    "api_secret": "WrLSXktwAYhKI4pXDXbMrXRkTN_LhtfW",
+    "api_key": "",
+    "api_secret": "",
     "return_attributes": "gender,age,smiling,headpose,facequality,blur,eyestatus,emotion,ethnicity,beauty,mouthstatus,eyegaze,skinstatus",
 }
